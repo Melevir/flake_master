3c3bc8a0109b13b6cf91194c782d16181f321f09
Ilya Lebedev <melevir@gmail.com>
2021-02-25 06:33:53 +0300

    allow comments in requirements files & upgrade pytest

--- flake_master/utils/requirements.py
+++ flake_master/utils/requirements.py
@@ -21,7 +21,10 @@ def merge_requirements_data(
 def find_requirement_in_list(package_name: str, raw_old_requirements: List[str]) -> Optional[int]:
     match_line_num = None
     for old_requirement_num, old_requirement in enumerate(raw_old_requirements):
-        parsed_requirement = Requirement.parse(old_requirement) if old_requirement.strip() else None
+        requirement_str = old_requirement.strip()
+        if requirement_str.startswith('#'):
+            continue
+        parsed_requirement = Requirement.parse(requirement_str) if requirement_str else None
         if parsed_requirement and parsed_requirement.name == package_name:
             match_line_num = old_requirement_num
     return match_line_num
